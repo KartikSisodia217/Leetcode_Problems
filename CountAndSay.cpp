d1dc0d58a6be039817c25369f51d0ad7ba4767af
KartikSisodia217 <sisodiakartik21@gmail.com>
2026-01-15 21:45:36 +0530

    my commit on 15/01/2025

---
+++ CountAndSay.cpp
@@ -0,0 +1,67 @@
+//This is my solution to Leetcode Problem #38: Count And Say. 
+
+/*
+
+The count-and-say sequence is a sequence of digit strings defined by the recursive formula:
+countAndSay(1) = "1"
+countAndSay(n) is the run-length encoding of countAndSay(n - 1).
+Run-length encoding (RLE) is a string compression method that works by replacing consecutive identical 
+characters (repeated 2 or more times) with the concatenation of the character and the number marking
+the count of the characters (length of the run). For example, to compress the string "3322251" we 
+replace "33" with "23", replace "222" with "32", replace "5" with "15" and replace "1" with "11". 
+Thus the compressed string becomes "23321511".
+Given a positive integer n, return the nth element of the count-and-say sequence.
+
+ 
+
+Example 1:
+Input: n = 4
+Output: "1211"
+Explanation:
+countAndSay(1) = "1"
+countAndSay(2) = RLE of "1" = "11"
+countAndSay(3) = RLE of "11" = "21"
+countAndSay(4) = RLE of "21" = "1211"
+
+
+Example 2:
+Input: n = 1
+Output: "1"
+Explanation:
+This is the base case.
+
+*/
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+    string countAndSay(int n) {
+        string s = "1";
+        for (int i = 2; i <= n; i++) {
+            string t = "";
+            for (int j = 0; j < s.size(); j++) {
+                int cnt = 1;
+                while (j + 1 < s.size() && s[j] == s[j + 1]) {
+                    cnt++;
+                    j++;
+                }
+                t += to_string(cnt);
+                t += s[j];
+            }
+            s = t;
+        }
+        return s;
+    }
+};
+
+int main(){
+  Solution sol;
+  int n;
+  cout<<"Enter the value of n: ";
+  cin>>n;
+  cout<< "CountAndSay("<<n<<") is : "<<sol.countAndSay(n)<<endl;
+  return 0;
+}
